b9ce7dba98db2d569f8eee3e46200d67100675cb
Drowrin <drowrin@gmail.com>
2023-12-05 21:56:19 -0500

    remove clones

--- day05b/src/solution.rs
+++ day05b/src/solution.rs
@@ -34,72 +34,70 @@ pub fn solution(input: &str) -> impl ToString {
             vec![(s, s + l - 1)]
         })
         .flat_map(|initial_ranges| {
-            maps.clone()
-                .into_iter()
-                .fold(initial_ranges, |ranges, map| {
-                    let mut remaining_ranges = ranges;
-                    let mut processed_ranges = vec![];
+            maps.iter().fold(initial_ranges, |ranges, map| {
+                let mut remaining_ranges = ranges;
+                let mut processed_ranges = vec![];
 
-                    for (map_src, map_dst, map_len) in map {
-                        let mut new_remaining = vec![];
+                for (map_src, map_dst, map_len) in map {
+                    let mut new_remaining = vec![];
 
-                        for (range_start, range_end) in remaining_ranges.clone() {
-                            if range_start >= map_src + map_len {
-                                // start is after map, entire range gets reprocessed
-                                new_remaining.push((range_start, range_end));
-                                continue;
-                            }
-                            if range_end < map_src {
-                                // end is before map, entire range gets reprocessed
-                                new_remaining.push((range_start, range_end));
-                                continue;
-                            }
-                            if range_start >= map_src {
-                                // start is within map
-                                if range_end < map_src + map_len {
-                                    // entire range is within map
-                                    // entire range gets mapped
-                                    processed_ranges.push((
-                                        map_dst + range_start - map_src,
-                                        map_dst + range_end - map_src,
-                                    ));
-                                } else {
-                                    // end is after map
-                                    // start of range until end of map gets mapped
-                                    processed_ranges.push((
-                                        map_dst + range_start - map_src,
-                                        map_dst + map_len - 1,
-                                    ));
-                                    // end of map until end of range goes back into unmapped ranges
-                                    new_remaining.push((map_src + map_len, range_end));
-                                }
+                    for (range_start, range_end) in remaining_ranges {
+                        if range_start >= *map_src + *map_len {
+                            // start is after map, entire range gets reprocessed
+                            new_remaining.push((range_start, range_end));
+                            continue;
+                        }
+                        if range_end < *map_src {
+                            // end is before map, entire range gets reprocessed
+                            new_remaining.push((range_start, range_end));
+                            continue;
+                        }
+                        if range_start >= *map_src {
+                            // start is within map
+                            if range_end < *map_src + *map_len {
+                                // entire range is within map
+                                // entire range gets mapped
+                                processed_ranges.push((
+                                    *map_dst + range_start - *map_src,
+                                    *map_dst + range_end - *map_src,
+                                ));
                             } else {
-                                // end is after map start
-                                if range_end >= map_src + map_len {
-                                    // range covers entire map
-                                    // map range gets mapped
-                                    processed_ranges.push((map_dst, map_dst + map_len - 1));
-                                    // end of map until end of range goes back into unmapped ranges
-                                    new_remaining.push((map_src + map_len, range_end));
-                                    // start of range until start of map goes back into unmapped ranges
-                                    new_remaining.push((range_start, map_src - 1));
-                                } else {
-                                    // end is within map
-                                    // start of map until end of range gets mapped
-                                    processed_ranges.push((map_dst, map_dst + range_end - map_src));
-                                    // start of range until start of map goes back into unmapped ranges
-                                    new_remaining.push((range_start, map_src - 1));
-                                }
+                                // end is after map
+                                // start of range until end of map gets mapped
+                                processed_ranges.push((
+                                    *map_dst + range_start - *map_src,
+                                    *map_dst + *map_len - 1,
+                                ));
+                                // end of map until end of range goes back into unmapped ranges
+                                new_remaining.push((*map_src + *map_len, range_end));
+                            }
+                        } else {
+                            // end is after map start
+                            if range_end >= *map_src + *map_len {
+                                // range covers entire map
+                                // map range gets mapped
+                                processed_ranges.push((*map_dst, *map_dst + *map_len - 1));
+                                // end of map until end of range goes back into unmapped ranges
+                                new_remaining.push((*map_src + *map_len, range_end));
+                                // start of range until start of map goes back into unmapped ranges
+                                new_remaining.push((range_start, *map_src - 1));
+                            } else {
+                                // end is within map
+                                // start of map until end of range gets mapped
+                                processed_ranges.push((*map_dst, *map_dst + range_end - *map_src));
+                                // start of range until start of map goes back into unmapped ranges
+                                new_remaining.push((range_start, *map_src - 1));
                             }
                         }
-
-                        remaining_ranges = new_remaining;
                     }
 
-                    processed_ranges.extend(remaining_ranges);
+                    remaining_ranges = new_remaining;
+                }
 
-                    processed_ranges
-                })
+                processed_ranges.extend(remaining_ranges);
+
+                processed_ranges
+            })
         })
         .min_by_key(|l| l.0)
         .unwrap()
