f6139e7bf0d8c2900e71ac69b43aaf48c7026299
Drowrin <drowrin@gmail.com>
2023-12-07 13:01:33 -0500

    day07

---
+++ day07b/src/solution.rs
@@ -0,0 +1,70 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn get_val(c: u8) -> u8 {
+    match c {
+        b'A' => 14,
+        b'K' => 13,
+        b'Q' => 12,
+        b'T' => 10,
+        b'9' => 9,
+        b'8' => 8,
+        b'7' => 7,
+        b'6' => 6,
+        b'5' => 5,
+        b'4' => 4,
+        b'3' => 3,
+        b'2' => 2,
+        b'J' => 0,
+        _ => panic!("Not a card character: {c}"),
+    }
+}
+
+fn get_strength(hand: &[u8]) -> u8 {
+    let mut cards = HashMap::<u8, u8>::new();
+
+    for card in hand {
+        cards.entry(*card).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    let jokers = cards.remove(&b'J').unwrap_or(0);
+
+    let mut best = cards.values().sorted().rev();
+
+    match best.next().unwrap_or(&0) + jokers {
+        5 => 6,
+        4 => 5,
+        3 => match best.next().unwrap() {
+            2 => 4,
+            _ => 3,
+        },
+        2 => match best.next().unwrap() {
+            2 => 2,
+            _ => 1,
+        },
+        _ => 0,
+    }
+}
+
+fn parse_hand(hand: &str) -> [u8; 6] {
+    let bytes = hand.as_bytes();
+    [
+        get_strength(bytes),
+        get_val(bytes[0]),
+        get_val(bytes[1]),
+        get_val(bytes[2]),
+        get_val(bytes[3]),
+        get_val(bytes[4]),
+    ]
+}
+
+pub fn solution(input: &str) -> impl ToString {
+    input
+        .split("\n")
+        .map(|line| line.split_whitespace().collect_tuple().unwrap())
+        .map(|(hand, bid)| (parse_hand(hand), bid.parse::<usize>().unwrap()))
+        .sorted_by_key(|(hand, _)| *hand)
+        .enumerate()
+        .map(|(i, (_, bid))| (1 + i) * bid)
+        .sum::<usize>()
+}
